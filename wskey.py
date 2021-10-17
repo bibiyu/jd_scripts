302f618bb89e82b227a23bc4b7f0f6a699924eab
feeey <667374@qq.com>
2021-10-17 16:45:53 +0800

    Update wskey.py

--- wskey.py
+++ wskey.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*
+#全民抢京豆（8.6-8.16)
 '''
+项目名称: JD-Script / jd_qjd
+功能：全民抢京豆（8.6-8.16）：https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0
+    满160豆需要20人助力，每个用户目前只能助力2次不同的用户。
+Date: 2021/7/3 上午10:02
+update: 2021.7.24 14:21
 建议cron: 1 0 * 8 *  python3 jd_qjd.py
 new Env('全民抢京豆 8.6-8.16');
+* 修复了助力活动不存在、增加了随机UA（如果未定义ua则启用随机UA）
+* 新增推送
+* 修复0点不能开团
+* 兼容pin为中文转码编码
 '''
 
 import socket
