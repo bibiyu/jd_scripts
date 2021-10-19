24b1d4cb60a0e0813052314a3d383df90d5b644a
feeey <667374@qq.com>
2021-10-19 19:58:02 +0800

    Add files via upload

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -29,8 +29,8 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let appId = '1EFRXxg' , homeDataFunPrefix = 'interact_template', collectScoreFunPrefix = 'harmony', message = ''
 let lotteryResultFunPrefix = homeDataFunPrefix, browseTime = 6
 const inviteCodes = [
-  '',
-  '',
+  'T0225KkcRhgQ91TQdkzzk6RYJQCjVQmoaT5kRrbA@',
+  'T0225KkcRhgQ91TQdkzzk6RYJQCjVQmoaT5kRrbA',
 ];
 const randomCount = $.isNode() ? 20 : 5;
 const notify = $.isNode() ? require('./sendNotify') : '';
