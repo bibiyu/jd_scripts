73e255f8ec0a4542396713a649c2139e6f853e13
feeey <667374@qq.com>
2021-11-01 19:24:45 +0800

    Add files via upload

--- jd_zzt.js
+++ jd_zzt.js
@@ -138,7 +138,7 @@ async function main() {
     console.log(`当前积分：${user.coins},可以摇骰子：${time}次`);
     for (let i = 0; i < time; i++) {
         console.log(`\n进行第${i+1}次摇骰子`);
-        let dice = await taskUrl("dice", "")
+        let dice = await taskUrl("dice_new", "")
         if (dice.prize_info && JSON.stringify(dice.prize_info) !== '[]'){
             if(dice.prize_info.type === 1){
                 console.log(`获得：${dice.prize_info.beans || 0}京豆`);
