43487081ffa877326d0550a641c3a064f9485574
feeey <667374@qq.com>
2021-11-08 12:29:44 +0800

    Add files via upload

--- jd_superBrand2.js
+++ jd_superBrand2.js
@@ -256,7 +256,7 @@ async function takeRequest(cookie,functionId,bodyInfo){
 function getShareCode() {
   return new Promise(resolve => {
     $.get({
-      url: `https://raw.githubusercontent.com/he1pu/params/main/codes.json?${new Date()}`,
+      //url: `https://raw.githubusercontent.com/he1pu/params/main/codes.json?${new Date()}`,
       headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
